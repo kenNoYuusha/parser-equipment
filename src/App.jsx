56b3aeca0797a5478f121163766f0b14cfb1d3f1
RenAmaki <jestevez1709@hotmail.com>
2026-05-16 14:44:03 -0600

    initializing proyect

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+ 
+  return (
+    <>
+     <p className='text-3xl font-bold underline'>Hola Parcer</p>
+     <p className="text-indigo-700  ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex facere pariatur dolore, hic aspernatur deleniti! Commodi pariatur sint officia, voluptates eos culpa natus inventore illo earum repudiandae similique eius vero.</p>
+    </>
+  )
+}
+
+export default App
